7daa26bcb2e81a718d240134724c1b84f724837d
Borislav Dukov <borislav.dukov@icloud.com>
2024-02-03 12:03:12 +0200

    Add CSS Flames
    Add flames to H1 in welcome page

--- src/containers/main/Home/Welcome.jsx
+++ src/containers/main/Home/Welcome.jsx
@@ -6,7 +6,17 @@ export default function Welcome() {
     <div className="container">
       <div className="banner">
         {/* <Link to={`/home`}> */}
-          <h1> Enter to Valhala!</h1>
+        <div className="fire">
+          <h1> Enter to Valhalla!</h1>
+          <svg>
+            <filter id="flame">
+              <feTurbulence id="turbulence" baseFrequency="0.1 0.1" numOctaves="3" seed="4">
+                <animate attributeName="baseFrequency" dur="8s" values="0.1 0.1; 0.13 0.13" repeatCount="indefinite"></animate>
+              </feTurbulence>
+              <feDisplacementMap in="SourceGraphic" scale="10"></feDisplacementMap>
+            </filter>
+          </svg>
+        </div>
           {/* </Link> */}
         <p>B.DUKOV 2024 &reg;</p>
       </div>
