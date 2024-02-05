cbcf64a9895f725e103e33d40f92fc31f60f55fa
Borislav Dukov <borislav.dukov@icloud.com>
2024-02-05 18:58:46 +0200

    remove fire from Login page

--- src/containers/user/Login/Login.jsx
+++ src/containers/user/Login/Login.jsx
@@ -1,7 +1,9 @@
+/* eslint-disable react/no-unescaped-entities */
 import Header from "../../../components/Header/Header";
 import Footer from "../../../components/Footer/Footer";
 
 import "./Login.css";
+import { Link } from "react-router-dom";
 
 export default function Login() {
   return (
@@ -9,79 +11,28 @@ export default function Login() {
       <Header />
       <div className="login-form">
         <form id="login">
-          {/* <div className="name">
-            <div className="fire">
-            <label htmlFor="">Name:</label>
-            <svg>
-            <filter id="flame">
-              <feTurbulence id="turbulence" baseFrequency="0.1 0.1" numOctaves="3" seed="4">
-                <animate attributeName="baseFrequency" dur="8s" values="0.1 0.1; 0.13 0.13" repeatCount="indefinite"></animate>
-              </feTurbulence>
-              <feDisplacementMap in="SourceGraphic" scale="4"></feDisplacementMap>
-            </filter>
-          </svg>
-            </div>
-            <br />
-            <input type="text" />
-            </div> */}
+
 
           <div className="email">
-            <div className="fire">
               <label htmlFor="">Email</label>
-              <svg>
-                <filter id="flame">
-                  <feTurbulence
-                    id="turbulence"
-                    baseFrequency="0.1 0.1"
-                    numOctaves="3"
-                    seed="4"
-                  >
-                    <animate
-                      attributeName="baseFrequency"
-                      dur="8s"
-                      values="0.1 0.1; 0.13 0.13"
-                      repeatCount="indefinite"
-                    ></animate>
-                  </feTurbulence>
-                  <feDisplacementMap
-                    in="SourceGraphic"
-                    scale="4"
-                  ></feDisplacementMap>
-                </filter>
-              </svg>
-            </div>
             <br />
             <input type="text" />
-          </div>
+            </div>
+
 
           <div className="password">
-            <div className="fire">
               <label htmlFor="">Password</label>
-              <svg>
-                <filter id="flame">
-                  <feTurbulence
-                    id="turbulence"
-                    baseFrequency="0.1 0.1"
-                    numOctaves="3"
-                    seed="4"
-                  >
-                    <animate
-                      attributeName="baseFrequency"
-                      dur="8s"
-                      values="0.1 0.1; 0.13 0.13"
-                      repeatCount="indefinite"
-                    ></animate>
-                  </feTurbulence>
-                  <feDisplacementMap
-                    in="SourceGraphic"
-                    scale="4"
-                  ></feDisplacementMap>
-                </filter>
-              </svg>
-            </div>
             <br />
             <input type="password" />
           </div>
+
+
+
+    <div className="button">
+          <button type="submit" className="btn">Login</button>
+            <p id="signUpMsg">Don't have an account? <Link>Register now!</Link></p>
+    </div>
+
         </form>
       </div>
       <Footer />
