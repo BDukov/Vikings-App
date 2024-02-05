a4229c74f157eb58802b56a84d1c4e0f5ea55bca
Borislav Dukov <borislav.dukov@icloud.com>
2024-02-05 21:11:09 +0200

    make login and register responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,18 @@
 import "./Header.css";
 import { Link } from "react-router-dom";
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faUser } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+library.add(faUser);
+
 export default function Header() {
   return (
     <div className="navigation">
       <div className="home">
         <Link to={`/`}>
-          <img src="./images/logo1.png" alt="" />
+          <img src="./images/logo1.png" alt="" id="logo" />
         </Link>
         <Link to={`/home`}>Home</Link>
       </div>
@@ -17,10 +23,12 @@ export default function Header() {
           <li>Timeline</li>
           <li>GIF-s</li>
           <li>Shop</li>
+          <li id="menu">Menu</li>
         </ul>
       </div>
 
       <div className="user">
+      <FontAwesomeIcon id="user-icon"icon="fa-regular fa-user" style={{color: "#fcfcfc",}} />
         <ul id="user">
           <Link to={`/login`}>
             <li>Login</li>
